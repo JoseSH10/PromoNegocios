cb9f7f7a053bb57efb31e178d1130692538c2d31
Nahum <nahum071103@gmail.com>
2025-04-01 14:16:43 -0600

    Firebase Analytics

--- app/src/main/java/com/example/promonegociosguachinango/MainActivity.kt
+++ app/src/main/java/com/example/promonegociosguachinango/MainActivity.kt
@@ -12,12 +12,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.promonegociosguachinango.ui.theme.PromonegociosGuachinangoTheme
+import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.analytics
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FirebaseApp.initializeApp(this) // Initialize Firebase
+
+        val analytics: FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de firebase completa")
+        analytics.logEvent("init", bundle)
+
         enableEdgeToEdge()
         setContent {
             PromonegociosGuachinangoTheme {
